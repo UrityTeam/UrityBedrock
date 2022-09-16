49ff3ff7e15008aa2702543b68065dfb84319b97
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-09-16 17:34:31 +0300

    Update Identifiers.js

--- src/network/mcpe/protocol/Identifiers.js
+++ src/network/mcpe/protocol/Identifiers.js
@@ -14,8 +14,8 @@
 \******************************************/
 
 class Identifiers {
-	static CURRENT_PROTOCOL = 534;
-	static MINECRAFT_VERSION = "1.19.10";
+	static CURRENT_PROTOCOL = 544;
+	static MINECRAFT_VERSION = "1.19.20";
 	static GAME_PACKET = 0xfe;
 	static LOGIN_PACKET = 0x01;
 	static PLAY_STATUS_PACKET = 0x02;
